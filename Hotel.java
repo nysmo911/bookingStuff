cbd6b3e47099e8c8c856b50739a63924b90fddf5
Elina <81986341+ElinaHossain@users.noreply.github.com>
2024-10-31 20:38:20 +0000

    "Added Javadoc comments to Hotel, Room, and RoomType classes to provide documentation for methods and fields."

--- Hotel.java
+++ Hotel.java
@@ -1,24 +1,46 @@
 import java.util.*;
 
+/**
+ * The Hotel class represents a hotel containing a collection of rooms.
+ * It provides methods to add rooms, retrieve all rooms, 
+ * get available rooms, and display hotel information.
+ */
 public class Hotel {
-    private String name;
-    private String location;
-    private List<Room> rooms;
+    private String name;    // The name of the hotel
+    private String location; // The location of the hotel
+    private List<Room> rooms; // List of rooms in the hotel
 
+    /**
+     * Constructor to create a Hotel object with a name and location.
+     * @param name the name of the hotel
+     * @param location the location of the hotel
+     */
     public Hotel(String name, String location) {
         this.name = name;
         this.location = location;
         this.rooms = new ArrayList<>();
     }
 
+    /**
+     * Adds a room to the hotel's list of rooms.
+     * @param room the room to be added
+     */
     public void addRoom(Room room) {
         rooms.add(room);
     }
 
+    /**
+     * Retrieves the list of all rooms in the hotel.
+     * @return the list of rooms
+     */
     public List<Room> getRooms() {
         return rooms;  
     }
 
+    /**
+     * Retrieves a list of available rooms in the hotel.
+     * @return the list of available rooms
+     */
     public List<Room> getAvailableRooms() {
         List<Room> availableRooms = new ArrayList<>();
         for (Room room : rooms) {
@@ -29,6 +51,9 @@ public class Hotel {
         return availableRooms;
     }
 
+    /**
+     * Displays the hotel's information, including the name, location, and rooms.
+     */
     public void displayHotelInfo() {
         System.out.println("Hotel Name: " + name);
         System.out.println("Location: " + location);
@@ -38,10 +63,18 @@ public class Hotel {
         }
     }
 
+    /**
+     * Retrieves the name of the hotel.
+     * @return the hotel name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Retrieves the location of the hotel.
+     * @return the hotel location
+     */
     public String getLocation() {
         return location;
     }
--- Room.java
+++ Room.java
@@ -1,29 +1,54 @@
 import java.util.*;
 
+/**
+ * The Room class represents a room in a hotel. It contains details 
+ * about the room's price, availability, and room type.
+ */
 public class Room {
-    private double price;
-    private boolean isAvailable;
-    private RoomType roomType; 
+    private double price;     // Price of the room per night
+    private boolean isAvailable; // Indicates if the room is available
+    private RoomType roomType; // Type of the room (e.g., Single, Double)
 
+    /**
+     * Constructor to create a Room object with price, availability, and room type.
+     * @param price the price of the room
+     * @param isAvailable whether the room is available
+     * @param roomType the type of the room
+     */
     public Room(double price, boolean isAvailable, RoomType roomType) {
         this.price = price;
         this.isAvailable = isAvailable;
         this.roomType = roomType;
     }
 
-
+    /**
+     * Retrieves the price of the room.
+     * @return the room price
+     */
     public double getPrice() {
         return price;
     }
 
+    /**
+     * Checks if the room is available.
+     * @return true if available, false otherwise
+     */
     public boolean isAvailable() {
         return isAvailable;
     }
 
+    /**
+     * Retrieves the room type.
+     * @return the room type
+     */
     public RoomType getRoomType() {
         return roomType;
     }
 
+    /**
+     * Returns a string representation of the room, including price, availability, and room type.
+     * @return a string describing the room
+     */
     public String toString() {
         return "Price: $" + price + ", Available: " + isAvailable + ", Room Type: " + roomType;
     }
