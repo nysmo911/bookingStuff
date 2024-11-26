5e59775ca4edc32060428bfb6a44de41d06d54bd
brandon brenes <brandonmbrenes2@gmail.com>
2024-11-26 14:24:40 -0800

    Created Generic DAO interface and hotelDAO implementation. Also, adjusted the hotel class private variables to closer match the database fields.

--- src/main/java/booking/DbConnection.java
+++ src/main/java/booking/DbConnection.java
@@ -18,7 +18,7 @@ import com.mongodb.client.MongoDatabase;
  *  @version 1.0
  *  @date 11/09/2024
  */
-public class dbConnection {
+public class DbConnection {
 
 
     /**
@@ -45,18 +45,18 @@ public class dbConnection {
     {
         settings = MongoClientSettings.builder()
                 .applyConnectionString(new ConnectionString(connectionString))
-                //  .serverApi(serverApi)
+                // .serverApi(serverApi)
                 .build();
     }
 
-    private static dbConnection instance;
+    private static DbConnection instance;
     private MongoClient mongoClient;
     private MongoDatabase database;
 
     /**
      * Private constructor
      */
-    private dbConnection() {
+    private DbConnection() {
         connect();
     }
 
@@ -64,9 +64,9 @@ public class dbConnection {
      * Gets the instance
      * @return instance
      */
-    public static dbConnection getInstance() {
+    public static DbConnection getInstance() {
         if (instance == null) {
-          instance = new dbConnection();
+          instance = new DbConnection();
         }
         return instance;
     }
