2967035a51a48678480e369a57bc76b47ea170c7
ElinaHossain <elina.hossain@gmail.com>
2024-11-30 15:11:19 -0800

    Combining Room and RoomType

--- src/main/java/booking/HotelListController.java
+++ src/main/java/booking/HotelListController.java
@@ -1,4 +1,30 @@
 package booking;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class HotelListController {
+
+    @FXML
+    private void handleViewProfile(ActionEvent event) {
+        try {
+            // Load the SignIn.fxml file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/booking/signInPage.fxml"));
+            Parent root = loader.load();
+
+            // Get the current stage and set the new scene
+            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Sign In");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
